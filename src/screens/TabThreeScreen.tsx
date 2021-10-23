7ce307a32ef4c2b1a78da40c48029775d878c6b2
ayoshida1101 <aukt1011161@icloud.com>
2021-10-24 01:08:16 +0900

    地図のピンをカスタマイズした

--- src/screens/TabThreeScreen.tsx
+++ src/screens/TabThreeScreen.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { View } from "../components/Themed";
 import MapView, { Marker, Polyline, Region } from "react-native-maps";
 import { Modalize } from "react-native-modalize";
-import { useWindowDimensions } from "react-native";
+import { Image, useWindowDimensions } from "react-native";
 import { useGetAPI } from "src/hooks/useGetAPI";
 
 type CoordinateType = {
@@ -15,6 +15,7 @@ type CoordinateType = {
 
 export default function TabThreeScreen() {
   const windowDimensions = useWindowDimensions();
+
   const [mapCorners, setMapCorners] = React.useState<{
     lat1: number;
     lat2: number;
@@ -22,13 +23,19 @@ export default function TabThreeScreen() {
     lon2: number;
   }>({ lat1: 45, lat2: 46, lon1: 135, lon2: 136 });
 
-  const getBounds = (region: Region) =>
+  const [imageSize, setImageSize] = React.useState<number>(50);
+
+  const getBounds = (region: Region) => {
     setMapCorners({
       lat1: region.longitude - region.longitudeDelta / 2,
       lat2: region.longitude + region.longitudeDelta / 2,
       lon1: region.latitude - region.latitudeDelta / 2,
       lon2: region.latitude + region.latitudeDelta / 2,
     });
+    setImageSize(
+      Math.max(50, Math.min((20 / region.longitudeDelta) * 0.7, 100))
+    );
+  };
 
   const { data } = useGetAPI("/album/detail", {
     // TODO: 実データに置き換える
@@ -41,21 +48,27 @@ export default function TabThreeScreen() {
   const coordinates: CoordinateType[] = [
     {
       id: "gpsId1",
-      imageUrls: ["https://"],
+      imageUrls: [
+        "https://www.nagoyajo.city.nagoya.jp/images/content/guide/tenshu/index_img01.jpg",
+      ],
       latitude: 35.1221702,
       longitude: 136.9682402,
       timestamp: "2021-10-05T12:34:56.123456+00:00",
     },
     {
       id: "gpsId2",
-      imageUrls: ["https://"],
+      imageUrls: [
+        "https://www.nagoyajo.city.nagoya.jp/images/content/guide/tenshu/index_img01.jpg",
+      ],
       latitude: 35.1221702,
       longitude: 136.9115014,
       timestamp: "2021-10-05T12:34:56.123456+00:00",
     },
     {
-      id: "gpsId4",
-      imageUrls: ["https://"],
+      id: "gpsId3",
+      imageUrls: [
+        "https://www.nagoyajo.city.nagoya.jp/images/content/guide/tenshu/index_img01.jpg",
+      ],
       latitude: 35.1159824,
       longitude: 136.9797153,
       timestamp: "2021-10-05T12:34:56.123456+00:00",
@@ -77,7 +90,50 @@ export default function TabThreeScreen() {
         style={{ flex: 1.0 }}
       >
         {coordinates.map((c) => (
-          <Marker coordinate={c} />
+          <Marker coordinate={c}>
+            <View
+              style={{
+                top: -35 - (imageSize - 50) / 2,
+                height: imageSize + 10,
+                width: imageSize + 10,
+                // alignSelf: "center",
+                // justifyContent: "center",
+                borderRadius: 4,
+                backgroundColor: "#36C1A7",
+              }}
+            >
+              <Image
+                resizeMode="cover"
+                source={{
+                  uri: c.imageUrls[0],
+                  height: imageSize,
+                  width: imageSize,
+                }}
+                style={{
+                  borderRadius: 4,
+                  position: "relative",
+                  top: 5,
+                  left: 5,
+                }}
+              />
+            </View>
+            <View
+              style={{
+                top: -35 - (imageSize - 50) / 2,
+                width: 0,
+                height: 0,
+                alignSelf: "center",
+                backgroundColor: "transparent",
+                borderLeftWidth: 10,
+                borderRightWidth: 10,
+                borderTopWidth: 20,
+                borderBottomWidth: 0,
+                borderTopColor: "#36C1A7",
+                borderLeftColor: "transparent",
+                borderRightColor: "transparent",
+              }}
+            />
+          </Marker>
         ))}
         <Polyline coordinates={coordinates} strokeWidth={3} strokeColor="red" />
       </MapView>
