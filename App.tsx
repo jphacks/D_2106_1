84a19225325a84686454651f1eabc360cbe40f09
Narikawa <38754367+TakaoNarikawa@users.noreply.github.com>
2021-10-23 10:54:46 +0900

    コンポーネントライブラリを追加

--- App.tsx
+++ App.tsx
@@ -5,6 +5,8 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import useCachedResources from "./src/hooks/useCachedResources";
 import useColorScheme from "./src/hooks/useColorScheme";
 import Navigation from "./src/navigation";
+import * as eva from "@eva-design/eva";
+import { ApplicationProvider, Layout, Text } from "@ui-kitten/components";
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -14,10 +16,12 @@ export default function App() {
     return null;
   } else {
     return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
+      <ApplicationProvider {...eva} theme={eva.light}>
+        <SafeAreaProvider>
+          <Navigation colorScheme={colorScheme} />
+          <StatusBar />
+        </SafeAreaProvider>
+      </ApplicationProvider>
     );
   }
 }
