1908a4baf8239712c0cfbff4d3b311e93bc85200
ayoshida1101 <aukt1011161@icloud.com>
2021-10-23 23:17:16 +0900

    マップにピンを表示

--- src/screens/TabThreeScreen.tsx
+++ src/screens/TabThreeScreen.tsx
@@ -1,11 +1,36 @@
 import * as React from "react";
 import { View } from "../components/Themed";
-import MapView from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
 import { Modalize } from "react-native-modalize";
 import { useWindowDimensions } from "react-native";
 
 export default function TabThreeScreen() {
   const windowDimensions = useWindowDimensions();
+
+  // TODO: 実データに置き換える
+  const coordinates = [
+    {
+      id: "gpsId1",
+      imageUrls: ["https://"],
+      latitude: 35.1221702,
+      longitude: 136.9682402,
+      timestamp: "2021-10-05T12:34:56.123456+00:00",
+    },
+    {
+      id: "gpsId2",
+      imageUrls: ["https://"],
+      latitude: 35.1221702,
+      longitude: 136.9115014,
+      timestamp: "2021-10-05T12:34:56.123456+00:00",
+    },
+    {
+      id: "gpsId4",
+      imageUrls: ["https://"],
+      latitude: 35.1159824,
+      longitude: 136.9797153,
+      timestamp: "2021-10-05T12:34:56.123456+00:00",
+    },
+  ];
 
   return (
     <View style={{ flex: 1.5 }}>
@@ -17,7 +42,11 @@ export default function TabThreeScreen() {
           longitudeDelta: 0.5,
         }}
         style={{ flex: 1.0 }}
-      ></MapView>
+      >
+        {coordinates.map((c) => (
+          <Marker coordinate={c} />
+        ))}
+      </MapView>
       <Modalize
         alwaysOpen={windowDimensions.height * 0.25}
         modalHeight={windowDimensions.height * 0.75}
