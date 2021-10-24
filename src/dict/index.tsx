c264d7ac2e6e9cdb5082a79c583950634a5bc1c4
ayoshida1101 <aukt1011161@icloud.com>
2021-10-24 19:44:15 +0900

    CreateNewAlbumFifth を定義

--- src/dict/index.tsx
+++ src/dict/index.tsx
@@ -3,4 +3,5 @@ export const screens = {
   CreateNewAlbumSecond: "/create-new-album/second",
   CreateNewAlbumThird: "/create-new-album/third",
   CreateNewAlbumFour: "/create-new-album/four",
+  CreateNewAlbumFifth: "/create-new-album/five",
 };
