6aab42a1e73ae83f5f0f580374f8b16c797614fd
KosakaNao00 <81952022+KosakaNao00@users.noreply.github.com>
2021-10-25 01:27:36 +0900

    Mypage(OneScreen) and SetProfile(TwoScreen)

--- src/screens/TabOneScreen.js
+++ src/screens/TabOneScreen.js
@@ -1,114 +1,89 @@
-import { Button, Text } from "@ui-kitten/components";
+import { Avatar, Button, Text } from "@ui-kitten/components";
 import React from "react";
-import { Dimensions, Linking, StyleSheet, View } from "react-native";
-import Image from "src/components/atoms/Image";
+import { FlatList, SafeAreaView, StyleSheet } from "react-native";
+import PostedCard from "src/components/atoms/PostedCard";
 import { Padding } from "src/components/layouts/Margin";
-import { BASE_PX } from "src/utils/space";
+import Space from "src/components/layouts/Space";
+
+const SampleUser = {
+  userid: "shinjibaka2021",
+  name: "真治",
+  profile_image_url:
+    "https://amd-pctr.c.yimg.jp/r/iwiz-amd/20211020-00000048-chuspo-000-2-view.jpg",
+  introduction:
+    "しんじ22歳　座右の銘は、「毎日が学び」                            #春から東京大学大学院 #有料物件 #学歴ロンダリングしてる人と繋がりたい #来世はゴリラになりたい #シャバーニ",
+};
+
+const sampleData = [
+  {
+    title: "動物園",
+    imageUrl:
+      "https://amd-pctr.c.yimg.jp/r/iwiz-amd/20211020-00000048-chuspo-000-2-view.jpg",
+    createdAt: ["Nagoya", "Mie", "Nara"],
+    timestamp: 1635071480,
+  },
+  {
+    title: "水族館",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Niphon_spinosus_Oga_Aquarium_2.jpg/1200px-Niphon_spinosus_Oga_Aquarium_2.jpg",
+    createdAt: ["Nagoya", "Mie", "Nara"],
+    timestamp: 1635071480,
+  },
+  {
+    title: "動物園",
+    imageUrl:
+      "https://amd-pctr.c.yimg.jp/r/iwiz-amd/20211020-00000048-chuspo-000-2-view.jpg",
+    createdAt: ["Nagoya", "Mie", "Nara"],
+    timestamp: 1635071480,
+  },
+  {
+    title: "水族館",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Niphon_spinosus_Oga_Aquarium_2.jpg/1200px-Niphon_spinosus_Oga_Aquarium_2.jpg",
+    createdAt: ["Nagoya", "Mie", "Nara"],
+    timestamp: 1635071480,
+  },
+];
 
 export default function TabOneScreen({ navigation }) {
-  const height = Dimensions.get("window").height;
-  const width = Dimensions.get("window").width;
-
+  const renderItem = ({ item }) => (
+    <PostedCard
+      title={item.title}
+      imageUrl={item.imageUrl}
+      createdAt={item.createdAt}
+      timestamp={item.timestamp}
+    />
+  );
   return (
-    <Padding size={BASE_PX} style={styles.flex1}>
-      <View style={styles.header}>
-        <Text category="h5" width={width}>
-          Mappinstagramを使用するために、位置情報の設定を変更してください
-        </Text>
-      </View>
-      <View style={styles.content}>
-        <View style={styles.steps}>
-          <View style={{ flex: 1 }}>
-            <Text category="h6" style={styles.paragraph}>
-              1
-            </Text>
-          </View>
-          <View style={{ flex: 10 }}>
-            <Text category="h6">
-              この画面の一番下のボタンから、設定画面を開いてください
-            </Text>
-          </View>
-        </View>
-        <View style={styles.steps}>
-          <View style={{ flex: 1 }}>
-            <Text category="h6" style={styles.paragraph}>
-              2
-            </Text>
-          </View>
-          <View style={{ flex: 10 }}>
-            <Text category="h6">「位置情報」をタップしてください</Text>
-          </View>
-        </View>
-        <Image
-          source={require("../assets/images/Location.jpeg")}
-          width={width - 50}
-          height={50}
-          resizeMode="contain"
-        />
-        <View style={styles.steps}>
-          <View style={{ flex: 1 }}>
-            <Text category="h6" style={styles.paragraph}>
-              3
-            </Text>
-          </View>
-          <View style={{ flex: 10 }}>
-            <Text category="h6">「常に」を選択してください</Text>
-          </View>
-        </View>
-        <Image
-          source={require("../assets/images/LocationMode.jpeg")}
-          width={width - 50}
-          height={height / 4}
-          resizeMode="contain"
-        />
-        <View style={styles.steps}>
-          <View style={{ flex: 1 }}>
-            <Text category="h6" style={styles.paragraph}>
-              4
-            </Text>
-          </View>
-          <View style={{ flex: 10 }}>
-            <Text category="h6">Mappinstagramをもう一度開いてください</Text>
-          </View>
-        </View>
-      </View>
-      <View style={styles.Button}>
-        <Button onPress={() => Linking.openURL("app-settings:")}>
-          設定画面を開く
-        </Button>
-      </View>
+    <Padding size={10}>
+      <FlatList
+        data={sampleData}
+        renderItem={renderItem}
+        ListHeaderComponent={
+          <Space direction="vertical" style={styles.plofile}>
+            <Space size="middle">
+              <Avatar
+                size="giant"
+                source={{ uri: SampleUser.profile_image_url }}
+              />
+              <Space direction="vertical" size={0}>
+                <Text category="h4">{SampleUser.name}</Text>
+                <Text>{SampleUser.userid}</Text>
+              </Space>
+            </Space>
+            <Text>{SampleUser.introduction}</Text>
+            <Button size="small">プロフィールを編集する</Button>
+          </Space>
+        }
+      />
     </Padding>
   );
 }
 
 const styles = StyleSheet.create({
-  flex1: { flex: 1 },
-  header: {
-    flexGrow: 0,
-    // backgroundColor: "white",
-    flexDirection: "row",
-    flexWrap: "wrap",
+  plofile: {
+    marginTop: 10,
+    marginBottom: 10,
   },
-  content: {
-    flexGrow: 1,
-    // backgroundColor: "silver",
-  },
-  text: {
-    margin: 2,
-  },
-  steps: {
-    flexGrow: 0,
-    flexShrink: 0,
-    flexDirection: "row",
-    marginTop: 20,
-  },
-  paragraph: {
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  button: {
-    flexGrow: 6,
-    alignItems: "center",
-    justifyContent: "center",
-  },
+  posts: {},
 });
