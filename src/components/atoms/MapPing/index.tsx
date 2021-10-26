8ee01368310be7a0336679a161b1a55f3291c662
KosakaNao00 <81952022+KosakaNao00@users.noreply.github.com>
2021-10-27 01:17:11 +0900

    MapPing
    propsとして、サイズとURLが必要

---
+++ src/components/atoms/MapPing/index.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { View, Image } from "react-native";
+
+type Props = {
+  uri?: string;
+  size?: number;
+};
+
+const MapPing = (props) => {
+  return (
+    <>
+      <View style={{ top: 0, bottom: 0, left: 0, right: 0 }}>
+        <Image
+          style={{
+            width: props.size * 1,
+            height: props.size * 1.2,
+          }}
+          source={require("../../../assets/images/PING.png")}
+        />
+        <View
+          style={{
+            top: -props.size * 1.2 + props.size / 36,
+            left: props.size / 33,
+          }}
+        >
+          <Image
+            style={{
+              width: props.size * 0.9,
+              height: props.size * 0.9,
+              position: "absolute",
+            }}
+            source={{
+              uri: props.uri,
+            }}
+          />
+        </View>
+      </View>
+    </>
+  );
+};
+export default MapPing;
