f97dd7ea0649b7ac162f78a2bb415fa9b3dbf8cd
ayoshida1101 <aukt1011161@icloud.com>
2021-10-30 07:42:09 +0900

    デモデータ修正

--- src/assets/demo/assets.ts
+++ src/assets/demo/assets.ts
@@ -2,19 +2,19 @@ import * as MediaLibrary from "expo-media-library";
 
 export const assetsData: MediaLibrary.Asset[] = [
   {
-    creationTime: 1522437259365,
+    creationTime: 1,
     duration: 0,
     filename: "IMG_0006.HEIC",
     height: 3024,
     id: "CC95F08C-88C3-4012-9D6D-64A413D254B3/L0/001",
     mediaSubtypes: ["hdr"],
     mediaType: "photo",
-    modificationTime: 1635115412366,
+    modificationTime: 1635510623000,
     uri: "https://www.nagoyajo.city.nagoya.jp/images/content/guide/tenshu/index_img01.jpg",
     width: 4032,
   },
   {
-    creationTime: 1522437259365,
+    creationTime: 4000000000,
     duration: 0,
     filename: "IMG_0006.HEIC",
     height: 3024,
