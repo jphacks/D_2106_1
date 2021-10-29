a5b9ddb991be6aef0ac64c34e0b22de1a01d8899
ayoshida1101 <aukt1011161@icloud.com>
2021-10-30 05:21:58 +0900

    コンポーネントなどを作成

---
+++ src/assets/demo/assets.ts
@@ -0,0 +1,28 @@
+import * as MediaLibrary from "expo-media-library";
+
+export const assetsData: MediaLibrary.Asset[] = [
+  {
+    creationTime: 1522437259365,
+    duration: 0,
+    filename: "IMG_0006.HEIC",
+    height: 3024,
+    id: "CC95F08C-88C3-4012-9D6D-64A413D254B3/L0/001",
+    mediaSubtypes: ["hdr"],
+    mediaType: "photo",
+    modificationTime: 1635115412366,
+    uri: "https://www.nagoyajo.city.nagoya.jp/images/content/guide/tenshu/index_img01.jpg",
+    width: 4032,
+  },
+  {
+    creationTime: 1522437259365,
+    duration: 0,
+    filename: "IMG_0006.HEIC",
+    height: 3024,
+    id: "CC95F08C-88C3-4012-9D6D-64A413D254B3/L0/002",
+    mediaSubtypes: ["hdr"],
+    mediaType: "photo",
+    modificationTime: 1635115412366,
+    uri: "https://www.nagoyajo.city.nagoya.jp/images/content/guide/tenshu/index_img01.jpg",
+    width: 4032,
+  },
+];
