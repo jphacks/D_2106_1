685da9a8071d4c11d6d06bebd9ae7b0f86b026e8
ayoshida1101 <aukt1011161@icloud.com>
2021-10-30 10:15:27 +0900

    移動時の位置が中心になるようにした

--- src/screens/TabFiveScreen/index.tsx
+++ src/screens/TabFiveScreen/index.tsx
@@ -151,7 +151,7 @@ const FifthScreen: React.FC<{ albumId: string }> = ({ albumId }) => {
     mapRef.current?.animateToRegion({
       ...currentRegion,
       longitude: c?.longitude,
-      latitude: c?.latitude,
+      latitude: c?.latitude - currentRegion.latitudeDelta * 0.125,
     });
     navigationRef?.navigate("ImageFlatList");
   };
